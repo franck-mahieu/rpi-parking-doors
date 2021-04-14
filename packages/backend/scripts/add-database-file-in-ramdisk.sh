5ad663a0fb8ff9eba6c2e33c4cf7061f846e5eb7
franck-mahieu <franck.mahieu59@gmail.com>
2021-04-14 16:06:15 +0200

    wip

--- packages/backend/scripts/add-database-file-in-ramdisk.sh
+++ packages/backend/scripts/add-database-file-in-ramdisk.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 # Don't forget to set this file executable with this command :
 # chmod +x ./add-database-file-in-ramdisk.sh
-\cp -r /home/pi/rpi-parking-doors/packages/backend/data/sqlite.db /mnt/ramdisk/sqlite.db
+SQLITE_DATABASE_FS_PATH=/home/pi/database/sqlite.db
+SQLITE_DATABASE_RAMDISK_PATH=/mnt/ramdisk/sqlite.db
+SQLITE_DATABASE_REPOSITORY_PATH=/home/pi/rpi-parking-doors/packages/backend/data/sqlite.db
+
+if [ -f $SQLITE_DATABASE_FS_PATH ]; then
+  \cp -r $SQLITE_DATABASE_FS_PATH $SQLITE_DATABASE_RAMDISK_PATH
+else
+  \cp -r $SQLITE_DATABASE_REPOSITORY_PATH $SQLITE_DATABASE_RAMDISK_PATH
+fi
